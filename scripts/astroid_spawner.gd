402e678c48334c3d41419720d90739bcc1ebd46d
DonB303 <birnepaul101@gmail.com>
2024-11-02 17:01:33 +0100

    AstroidUpdate3.0

--- scripts/astroid_spawner.gd
+++ scripts/astroid_spawner.gd
@@ -1,8 +1,19 @@
 extends Node
-@onready var spawn_position: Marker2D = $Marker2D
 
+@onready var spawn_position: Marker2D = $Marker2D
+@onready var spawn_timer: Timer = $spawn_timer
+@onready var wave_timer: Timer = $wave_timer
+
+const ASTEROID = preload("res://Scenes/astroids/astroid.tscn")
 
 func spawn():
-	const ASTROID = preload("res://Scenes/astroids/astroid.tscn")
-	var new_astroid = ASTROID.instantiate()
-	new_astroid.global_position = spawn_position
+	var new_asteroid = ASTEROID.instantiate()
+	new_asteroid.global_position = spawn_position.global_position
+	get_parent().add_child(new_asteroid) 
+
+func _on_spawn_timer_timeout() -> void:
+	spawn()
+
+func _on_wave_timer_timeout() -> void:
+	spawn_timer.wait_time = max(0.1, spawn_timer.wait_time - 0.25) 
+	wave_timer.wait_time += 30
