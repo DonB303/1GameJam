e9466d788b081b845ca98b8440d74a890407d756
Ashley <ashleysbraindump@gmail.com>
2024-11-02 21:22:06 +0100

    beginofscore

--- scripts/astroids/astroid_spawner.gd
+++ scripts/astroids/astroid_spawner.gd
@@ -1,10 +1,8 @@
 extends Node2D
+
 @onready var spawn_position: Marker2D = $Marker2D
 @onready var spawn_timer: Timer = $spawn_timer
 @onready var wave_timer: Timer = $wave_timer
-
-
-
 
 func spawn():
 	const ASTROID = preload("res://Scenes/astroids/astroid.tscn")
@@ -12,12 +10,9 @@ func spawn():
 	new_astroid.global_position = spawn_position.global_position
 	spawn_position.add_child(new_astroid)
 
-
-
 func _on_spawn_timer_timeout() -> void:
 	spawn()
 	spawn_timer.start()
-
 
 func _on_wave_timer_timeout() -> void:
 	spawn_timer.wait_time -= 0.25
